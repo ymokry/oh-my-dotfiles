724318b0be6f1801d98d524d570a896450faceb1
Yauheni Mokry <yauheni.mokry@gmail.com>
2024-01-16 01:42:44 +0100

    [16-01-2024] Add xcode-select install

--- setup.sh
+++ setup.sh
@@ -2,5 +2,18 @@
 
 echo "Starting Mac setup..."
 
+# Request and keep the administrator password active.
+sudo -v
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# XCode Command Line Tools: Install if not already present.
+echo "Checking for Xcode Command Line Tools..."
+if ! xcode-select -p >/dev/null 2>&1; then
+    echo "Installing Xcode Command Line Tools..."
+    xcode-select --install
+else
+    echo "Xcode Command Line Tools already installed."
+fi
+
 echo "Mac setup completed."
 read -p "Press [Enter] to exit"
