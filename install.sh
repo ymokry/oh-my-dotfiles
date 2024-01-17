d21e301b31b07cfe9c1ee0d33d877a29c2507681
Yauheni Mokry <yauheni.mokry@gmail.com>
2024-01-17 11:59:32 +0100

    [17-01-2024] Merge installation scripts

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-echo "Installing Oh My Dotfiles..."
+echo "Starting Mac setup..."
 
 # Request and keep the administrator password active.
 sudo -v
@@ -11,7 +11,7 @@ echo "Checking for Xcode Command Line Tools..."
 if ! xcode-select -p >/dev/null 2>&1; then
     echo "Installing Xcode Command Line Tools..."
     xcode-select --install
-    read -p "Press [Enter] after installation is finished to continue"
+    read -p "Press [Enter] after installation is finished"
 else
     echo "Xcode Command Line Tools already installed."
 fi
@@ -34,7 +34,7 @@ fi
 echo "Cloning Oh My Dotfiles repository..."
 OH_MY_DOTFILES_NAME="oh-my-dotfiles"
 OH_MY_DOTFILES_REPO="ymokry/$OH_MY_DOTFILES_NAME"
-export OH_MY_DOTFILES_PATH="$HOME/.$OH_MY_DOTFILES_NAME"
+OH_MY_DOTFILES_PATH="$HOME/.$OH_MY_DOTFILES_NAME"
 
 git clone "https://github.com/$OH_MY_DOTFILES_REPO.git" $OH_MY_DOTFILES_PATH
 
@@ -42,4 +42,45 @@ cd $OH_MY_DOTFILES_PATH
 git remote remove origin
 git remote add origin "git@github.com:$OH_MY_DOTFILES_REPO.git"
 
-source setup.sh
+# Homebrew Installation: Install Homebrew if not already installed.
+echo "Checking for Homebrew..."
+if ! command -v brew >/dev/null 2>&1; then
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+else
+    echo "Homebrew already installed."
+fi
+
+# Update and Upgrade Homebrew: Ensure Homebrew is up-to-date.
+echo "Updating and Upgrading Homebrew..."
+brew update
+brew upgrade
+
+# Setup ZSH shell
+echo "Setting up ZSH shell..."
+brew install zsh
+OH_MY_DOTFILES_SHELL="$(brew --prefix)/bin/zsh"
+sudo sh -c "echo $OH_MY_DOTFILES_SHELL >> /etc/shells"
+chsh -s $OH_MY_DOTFILES_SHELL
+
+# Install Oh My ZSH
+echo "Setting up Oh My ZSH..."
+RUNZSH=no /bin/sh -c "$(curl -fsSL http://install.ohmyz.sh)"
+
+# Core Applications Installation: Install essential applications using Homebrew.
+echo "Installing core applications..."
+brew bundle install --no-lock
+
+# Clean up.
+echo "Running brew cleanup..."
+brew cleanup
+
+# Linking dotfiles
+echo "Setting up SSH..."
+OH_MY_DOTFILES_SSH="~/.ssh"
+mkdir -p $OH_MY_DOTFILES_SSH
+ln -s $OH_MY_DOTFILES_PATH/config/ssh/config $OH_MY_DOTFILES_SSH/config
+
+echo "Mac setup completed."
+read -p "Press [Enter] to exit"
