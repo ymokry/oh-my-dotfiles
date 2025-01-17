83f9e3875f3edb0ce9fbbf040657981bde342a06
Yauheni Mokry <github@sl.mokry.me>
2025-01-17 20:33:36 +0100

    Export XDG_CONFIG_HOME

--- zsh/.config/zsh/env.zsh
+++ zsh/.config/zsh/env.zsh
@@ -1,6 +1,7 @@
 # Disable .zsh_sessions
 export SHELL_SESSIONS_DISABLE=1
 
+export XDG_CONFIG_HOME="$HOME/.config"
 export PATH="$HOME/.local/bin:$PATH"
 
 if [[ -f "$ZSH_CONFIG/env.secret.zsh" ]]; then
