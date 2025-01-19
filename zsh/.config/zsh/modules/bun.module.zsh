7acff865bbd731adaf81b4a6b3e79ff4e8921c30
Yauheni Mokry <github@sl.mokry.me>
2025-01-19 11:21:49 +0100

    Fix bun completions

--- zsh/.config/zsh/modules/bun.module.zsh
+++ zsh/.config/zsh/modules/bun.module.zsh
@@ -1,7 +1,7 @@
 function _init_bun() {
     if (( $+commands[bun] )); then
         # Create if doesn't exist yet
-        if [ ! -s "$HOME/.bun/_bun"]; then
+        if [ ! -s "$HOME/.bun/_bun" ]; then
             bun completions
         fi
 
