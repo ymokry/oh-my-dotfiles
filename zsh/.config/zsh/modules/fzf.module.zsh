84b1d44760a1bf13ce3772a49d3c183717197b28
Yauheni Mokry <github@sl.mokry.me>
2025-01-19 14:39:50 +0100

    Integrate fd with fzf

--- zsh/.config/zsh/modules/fzf.module.zsh
+++ zsh/.config/zsh/modules/fzf.module.zsh
@@ -2,7 +2,7 @@ function _init_fzf() {
     if (( $+commands[fzf] )); then
         # Apply 'tokyonight_night' color theme
         # https://github.com/folke/tokyonight.nvim/blob/main/extras/fzf/tokyonight_night.sh
-        export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+        export FZF_DEFAULT_OPTS='
             --layout=reverse
             --preview-window="border-rounded"
             --border="rounded"
@@ -29,6 +29,22 @@ function _init_fzf() {
             --color=scrollbar:#27a1b9
             --color=separator:#ff9e64
             --color=header:#ff9e64'
+
+        export FZF_DEFAULT_COMMAND='fd --type f --strip-cwd-prefix --hidden --follow --exclude .git'
+        export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+        export FZF_ALT_C_COMMAND='fd --type d --strip-cwd-prefix --hidden --follow --exclude .git'
+
+        # Use fd (https://github.com/sharkdp/fd) for listing path candidates.
+        # - The first argument to the function ($1) is the base path to start traversal
+        # - See the source code (completion.{bash,zsh}) for the details.
+        _fzf_compgen_path() {
+            fd --hidden --follow --exclude ".git" . "$1"
+        }
+
+        # Use fd to generate the list for directory completion
+        _fzf_compgen_dir() {
+            fd --type d --hidden --follow --exclude ".git" . "$1"
+        }
 
         eval "$(fzf --zsh)"
     fi
