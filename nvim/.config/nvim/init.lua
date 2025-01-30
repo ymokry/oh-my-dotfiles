6a4e681fea9a0a62b0407a34545f871909582b4a
Yauheni Mokry <github@sl.mokry.me>
2025-01-30 01:27:03 +0100

    Add Neovim

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,7 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+-- NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- vim: ts=4 sts=4 sw=4 et
