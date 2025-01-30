e515752a33084761854b1d5db266538e14c96347
Yauheni Mokry <github@sl.mokry.me>
2025-01-30 17:53:32 +0100

    Update format plugin setup

--- nvim/.config/nvim/lua/plugins/format.lua
+++ nvim/.config/nvim/lua/plugins/format.lua
@@ -21,16 +21,13 @@ return {
             lsp_format = 'fallback',
         },
         -- Set up format-on-save
-        format_on_save = function(bufnr)
-            return {
-                timeout_ms = 500,
-            }
-        end,
+        format_on_save = {
+            timeout_ms = 500,
+        },
         -- Define your formatters
         formatters_by_ft = {
             lua = { 'stylua' },
-            python = { 'isort', 'black' },
-            javascript = { 'prettierd', 'prettier', stop_after_first = true },
+            javascript = { 'prettier' },
         },
     },
 }
