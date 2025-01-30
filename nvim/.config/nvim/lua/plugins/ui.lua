6ce1147157614221d6ed000dfa70b02c1334389f
Yauheni Mokry <github@sl.mokry.me>
2025-01-31 00:20:11 +0100

    Add basic UI plugins

---
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,17 @@
+return {
+    -- Adds git related signs to the gutter, as well as utilities for managing changes
+    {
+        'lewis6991/gitsigns.nvim',
+        opts = {
+            signs = {
+                add = { text = '+' },
+                change = { text = '~' },
+                delete = { text = '_' },
+                topdelete = { text = '‾' },
+                changedelete = { text = '~' },
+            },
+        },
+    },
+}
+
+-- vim: ts=4 sts=4 sw=4 et
