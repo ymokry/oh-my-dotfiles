32b1cf00fc16a524e34b703fca03b7a0805ee232
Yauheni Mokry <github@sl.mokry.me>
2025-01-30 01:28:47 +0100

    Add nvim alias

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,4 +1,6 @@
-alias ls='eza --color=always'
-alias c='clear'
-alias g='lazygit'
-alias t='[ -z "$TMUX" ] && { tmux has-session 2>/dev/null && tmux attach || tmux new-session; }'
+alias v="nvim"
+alias ls="eza --color=always"
+alias c="clear"
+alias g="lazygit"
+alias t="[ -z "$TMUX" ] && { tmux has-session 2>/dev/null && tmux attach || tmux new-session; }"
+
