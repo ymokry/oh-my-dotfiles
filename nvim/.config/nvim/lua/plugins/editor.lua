ed22bd1c177c5ab3617779968f0fb52d4fd6d859
Yauheni Mokry <github@sl.mokry.me>
2025-01-31 00:31:41 +0100

    Add comments highlights

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -28,6 +28,13 @@ return {
             },
         },
     },
+    -- Highlight todo, notes, etc in comments
+    {
+        'folke/todo-comments.nvim',
+        event = 'VimEnter',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        opts = { signs = false },
+    },
 }
 
 -- vim: ts=4 sts=4 sw=4 et
