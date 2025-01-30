c9b2728d449ec2015b743b62456a1a8ba4c71871
Yauheni Mokry <github@sl.mokry.me>
2025-01-30 17:25:00 +0100

    Add completions

---
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -0,0 +1,36 @@
+return {
+    'saghen/blink.cmp',
+    dependencies = {
+        'folke/lazydev.nvim',
+    },
+    -- NOTE: Use a release tag to download pre-built binaries
+    version = '*',
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+        -- 'default' for mappings similar to built-in completion
+        -- NOTE: See the full "keymap" documentation for information on defining your own keymap.
+        keymap = { preset = 'default' },
+        appearance = {
+            -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+            -- Adjusts spacing to ensure icons are aligned
+            nerd_font_variant = 'mono',
+        },
+        -- Default list of enabled providers defined so that you can extend it
+        -- elsewhere in your config, without redefining it, due to `opts_extend`
+        sources = {
+            default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+            providers = {
+                lazydev = {
+                    name = 'LazyDev',
+                    module = 'lazydev.integrations.blink',
+                    -- Make lazydev completions top priority (see `:h blink.cmp`)
+                    score_offset = 100,
+                },
+            },
+        },
+    },
+    opts_extend = { 'sources.default' },
+}
+
+-- vim: ts=4 sts=4 sw=4 et
