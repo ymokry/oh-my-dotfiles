fd75387304f6f99894a1dcae24ee78adc79445b0
Yauheni Mokry <github@sl.mokry.me>
2025-02-03 03:31:23 +0100

    Add optional formatting toggler

--- nvim/.config/nvim/lua/plugins/format.lua
+++ nvim/.config/nvim/lua/plugins/format.lua
@@ -21,9 +21,13 @@ return {
             lsp_format = 'fallback',
         },
         -- Set up format-on-save
-        format_on_save = {
-            timeout_ms = 500,
-        },
+        format_on_save = function(bufnr)
+            -- Disable with a global or buffer-local variable
+            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                return
+            end
+            return { timeout_ms = 500, lsp_format = 'fallback' }
+        end,
         -- Define your formatters
         formatters_by_ft = {
             lua = { 'stylua', stop_after_first = true },
