7d2c04e05b086c8ec5d89969b468b8dc542ac0f4
Yauheni Mokry <github@sl.mokry.me>
2025-02-03 03:30:57 +0100

    Drop broken condition

--- zsh/.config/zsh/modules/rust.module.zsh
+++ zsh/.config/zsh/modules/rust.module.zsh
@@ -2,11 +2,6 @@ function _init_rust {
     if (( $+commands[rustup] )); then
         # Add `rustup` modules to PATH
         PATH="$HOMEBREW_PREFIX/opt/rustup/bin:$PATH"
-
-        # Make sure `rustup` is initialized
-        if ! command -v cargo &> /dev/null; then
-            rustup default stable
-        fi
     fi
 
     unfunction _init_rust
