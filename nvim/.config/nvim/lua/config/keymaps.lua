167e38b0a1e5806c9588a1fe7571722c32c6cf7f
Yauheni Mokry <github@sl.mokry.me>
2025-02-05 22:49:02 +0100

    Add some usefull remaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -8,6 +8,24 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostics keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics [Q]uickfix list' })
 
+-- Move highlighted region in Visual mode
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- Easy navigation through large files
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+
+-- Keep cursor centered wnen navigatin search results
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'n', 'Nzzzv')
+
+-- Raplace selected region preserving the clipboard
+vim.keymap.set('x', '<leader>p', '"_dP')
+
+-- Delete preserving the clipboard content
+vim.keymap.set({ 'n', 'v' }, '<leader>d', '"_d')
+
 -- Disable arrow keys navigation
 vim.keymap.set('n', '<left>', "<cmd>echo 'Use h to move!'<CR>")
 vim.keymap.set('n', '<down>', "<cmd>echo 'Use j to move!'<CR>")
