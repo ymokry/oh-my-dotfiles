542dacbfe94c90f1276a2d380c249bbd7d7f0e90
Yauheni Mokry <github@sl.mokry.me>
2025-02-11 02:21:27 +0100

    Split mini in parts

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,26 +1,42 @@
 return {
-    -- Collection of various small independent plugins/modules
+    -- Better Around/Inside textobjects
+    --
+    -- Examples:
+    --  - va)  - [V]isually select [A]round [)]paren
+    --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+    --  - ci'  - [C]hange [I]nside [']quote
     {
-        'echasnovski/mini.nvim',
+        'echasnovski/mini.ai',
+        version = false,
+        event = 'VeryLazy',
+        opts = {
+            n_lines = 500,
+        },
+    },
+    -- Add/delete/replace surroundings (brackets, quotes, etc.)
+    --
+    -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+    -- - sd'   - [S]urround [D]elete [']quotes
+    -- - sr)'  - [S]urround [R]eplace [)] [']
+    {
+        'echasnovski/mini.surround',
+        version = false,
+        event = 'VeryLazy',
+        opts = {},
+    },
+    -- Minimal and fast autopairs
+    {
+        'echasnovski/mini.pairs',
+        version = false,
+        event = 'VeryLazy',
+        opts = {},
+    },
+    -- Statusline
+    {
+        'echasnovski/mini.statusline',
+        version = false,
+        event = 'VimEnter',
         config = function()
-            -- Better Around/Inside textobjects
-            --
-            -- Examples:
-            --  - va)  - [V]isually select [A]round [)]paren
-            --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-            --  - ci'  - [C]hange [I]nside [']quote
-            require('mini.ai').setup { n_lines = 500 }
-
-            -- Add/delete/replace surroundings (brackets, quotes, etc.)
-            --
-            -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-            -- - sd'   - [S]urround [D]elete [']quotes
-            -- - sr)'  - [S]urround [R]eplace [)] [']
-            require('mini.surround').setup()
-
-            -- Minimal and fast autopairs
-            require('mini.pairs').setup()
-
             -- Simple and easy statusline.
             local statusline = require 'mini.statusline'
             statusline.setup { use_icons = true }
