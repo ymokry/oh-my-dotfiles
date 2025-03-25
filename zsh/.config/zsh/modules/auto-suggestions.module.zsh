2732cc67a00e447304278b2a75e41dc374861285
Yauheni Mokry <github@sl.mokry.me>
2025-03-25 18:30:57 +0100

    Replace zsh-autosuggestions

--- zsh/.config/zsh/modules/auto-suggestions.module.zsh
+++ zsh/.config/zsh/modules/auto-suggestions.module.zsh
@@ -1,11 +1,10 @@
 function _init_auto_suggestions {
-    if (( $+commands[brew] )); then
-        local plugin="$HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
+    local plugin="$ZSH_CONFIG/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
 
-        if [ -f "$plugin" ]; then
-            source $plugin
-        fi
+    if [ -f "$plugin" ]; then
+        source $plugin
     fi
+
     unfunction _init_auto_suggestions
 }
 
