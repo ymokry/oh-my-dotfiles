4b30524dedf5e47248415731f03b22083b2bf7b9
Yauheni Mokry <github@sl.mokry.me>
2025-03-25 18:55:58 +0100

    Replace zsh-syntax-highlighting

--- zsh/.config/zsh/modules/syntax-highlighting.module.zsh
+++ zsh/.config/zsh/modules/syntax-highlighting.module.zsh
@@ -1,11 +1,10 @@
 function _init_syntax-highlighting {
-    if (( $+commands[brew] )); then
-        local plugin="$(brew --prefix zsh-fast-syntax-highlighting)/share/zsh-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
+    local plugin="$ZSH_CONFIG/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
 
-        if [ -f "$plugin" ]; then
-            source $plugin
-        fi
+    if [ -f "$plugin" ]; then
+        source $plugin
     fi
+
     unfunction _init_syntax-highlighting
 }
 
