2047d286d6e68d28dc607941ac0b528d7ef2ce07
Yauheni Mokry <github@sl.mokry.me>
2025-03-25 18:46:14 +0100

    Replace zsh-completions

--- zsh/.config/zsh/modules/completions.module.zsh
+++ zsh/.config/zsh/modules/completions.module.zsh
@@ -1,16 +1,8 @@
 function _init_completions {
-    if (( $+commands[brew] )); then
-        local completions="$(brew --prefix)/share/zsh-completions"
+    local completions="$ZSH_CONFIG/plugins/zsh-completions/src"
 
-        if [ -d "$completions" ]; then
-            # Check permissions and fix if needed
-            if [[ ! "$(ls -ld $HOMEBREW_PREFIX/share)" =~ ^d(rwx)(r-x)(r-x) ]]; then
-                chmod go-w "$HOMEBREW_PREFIX/share"
-                chmod -R go-w "$HOMEBREW_PREFIX/share/zsh"
-            fi
-
-            FPATH="$completions:$FPATH"
-        fi
+    if [ -d "$completions" ]; then
+        FPATH="$completions:$FPATH"
     fi
 
     # Case insensitive completions
