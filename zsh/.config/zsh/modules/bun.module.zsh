6b5d60888446c4caf8200c17c7184bceb0983ab2
Yauheni Mokry <github@sl.mokry.me>
2025-03-27 15:57:51 +0100

    Update bun initialization

--- zsh/.config/zsh/modules/bun.module.zsh
+++ zsh/.config/zsh/modules/bun.module.zsh
@@ -1,12 +1,18 @@
 function _init_bun() {
-    if (( $+commands[bun] )); then
+    export BUN_INSTALL="$HOME/.local/bun"
+    export PATH="$BUN_INSTALL/bin:$PATH"
+
+    if command -v bun &>/dev/null; then
+        local completions="$BUN_INSTALL/_bun"
+
         # Create if doesn't exist yet
-        if [ ! -s "$HOME/.bun/_bun" ]; then
-            bun completions
+        if [ ! -s "$completions" ]; then
+            bun completions >$completions
         fi
 
-        source $HOME/.bun/_bun
+        source $completions
     fi
+
     unfunction _init_bun
 }
 
