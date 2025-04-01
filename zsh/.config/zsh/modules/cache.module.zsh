628d9db224c79e3827ecdc13e80e3b963217c320
Yauheni Mokry <github@sl.mokry.me>
2025-04-01 19:31:21 +0200

    Fix completions cache

--- zsh/.config/zsh/modules/cache.module.zsh
+++ zsh/.config/zsh/modules/cache.module.zsh
@@ -1,18 +1,18 @@
 function _init_cache {
-    local zsh_cache="$XDG_CACHE_HOME/zsh"
+    export ZDOTDIR="$XDG_CACHE_HOME/zsh"
 
     # Check 'zsh' cache directory
-    if [ ! -d "$zsh_cache" ]; then
-        mkdir -p $zsh_cache
+    if [ ! -d "$ZDOTDIR" ]; then
+        mkdir -p $ZDOTDIR
     fi
 
     # Disable .zsh_sessions
     export SHELL_SESSIONS_DISABLE=1
 
     # Move completions dump to 'zsh' cache
-    zstyle ':completion:*' dumpfile $zsh_cache/.zcompdump
+    zstyle ':completion:*' dumpfile "$ZDOTDIR/.zcompdump"
     # Move completions cache to 'zsh' cache
-    zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
+    zstyle ':completion:*' cache-path "$ZDOTDIR/.zcompcache"
 
     unfunction _init_cache
 }
--- zsh/.config/zsh/modules/completions.module.zsh
+++ zsh/.config/zsh/modules/completions.module.zsh
@@ -23,7 +23,7 @@ function _init_completions {
     zstyle ':fzf-tab:*' use-fzf-default-opts yes
 
     # Load completions
-    autoload -Uz compinit && compinit -d $XDG_CACHE_HOME/zsh/.zcompdump
+    autoload -Uz compinit && compinit -d "$ZDOTDIR/.zcompdump"
 
     unfunction _init_completions
 }
