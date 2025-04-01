6106049699a83177db6a530358d00d4cbf9c7184
Yauheni Mokry <github@sl.mokry.me>
2025-04-01 20:56:53 +0200

    Add JS/TS debug macro

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,18 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup('config_' .. name, { clear = true })
+end
+
+-- Load debug command to @l register for JS/TS files
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup 'js_debug',
+  pattern = { 'javascript', 'typescript' },
+  callback = function()
+    local esc_key = vim.api.nvim_replace_termcodes('<Esc>', true, true, true)
+
+    vim.fn.setreg('l', 'yoconsole.log("' .. esc_key .. 'pa:' .. esc_key .. 'la, ' .. esc_key .. 'pla;' .. esc_key)
+  end,
+})
