3abfad5d843753fd1d796bd5045778b67b87fc3b
Yauheni Mokry <git@sl.mokry.me>
2025-04-07 09:46:30 +0200

    Add quit alias

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -3,4 +3,4 @@ alias ls="eza --color=always"
 alias c="clear"
 alias g="lazygit"
 alias t="[ -z "$TMUX" ] && { tmux has-session 2>/dev/null && tmux attach || tmux new-session; }"
-
+alias q="[ -n "$TMUX" ] && { tmux detach; } || { exit; }"
